75a55d2f5735765b738f2c917b9d55a92114dd44
shubhzzzxxx <shubhzzz.xx@gmail.com>
2022-11-02 21:18:13 +0530

    created a prime

---
+++ Prime_Problem.js
@@ -0,0 +1,17 @@
+//print details
+
+let details= {
+  name: "Shubham",
+  age: 21,
+  sex: "Male",
+  location: "Ujjain"
+};
+
+// bracket noation
+  console.log(name["location"]);
+  console.log(name["sex"]);
+
+//dot notation
+  console.log(name.name);
+  console.log(name.age);
+
